0bc3767378a6b031f701d63beb6ba6f870175d56
paprckoviktorie <paprckovaviktorie@gmail.com>
2023-10-06 08:53:15 +0200

    Finální kalkulačka

--- KalkulackaInteraktiv/Form1.Designer.cs
+++ KalkulackaInteraktiv/Form1.Designer.cs
@@ -40,8 +40,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonScitani
@@ -151,34 +154,65 @@
             this.label3.Size = new System.Drawing.Size(82, 25);
             this.label3.TabIndex = 11;
             this.label3.Text = "Výsledek";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(60, 38);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(528, 31);
-            this.textBox3.TabIndex = 12;
-            this.textBox3.Text = "Při odmocňování a převodu na binární číslo piš pouze první číslo";
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(415, 272);
+            this.button1.Location = new System.Drawing.Point(415, 258);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(259, 56);
+            this.button1.Size = new System.Drawing.Size(317, 56);
             this.button1.TabIndex = 13;
             this.button1.Text = "převod na binární soustavu";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(415, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(317, 56);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "převod na hexadecimální soustavu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(415, 382);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(317, 56);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "převod na oktální soustavu";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(461, 25);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Při odmocňování a převodech piš číslo pouze první číslo.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(58, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(496, 25);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Při desetinných číslech piš desetinnou čárku a nedělej mezery.";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -212,7 +246,10 @@
         private Label label1;
         private Label label2;
         private Label label3;
-        private TextBox textBox3;
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label4;
+        private Label label5;
     }
 }
--- KalkulackaInteraktiv/Form1.cs
+++ KalkulackaInteraktiv/Form1.cs
@@ -2,18 +2,14 @@ namespace KalkulackaInteraktiv
 {
     public partial class Form1 : Form
     {
-        double prvni;
+        double prvni; //První promìnná je potøebná ve všech možnostech, deklaruji si ji tedy už tady.
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonScitani_Click(object sender, EventArgs e)
+        private void buttonScitani_Click(object sender, EventArgs e) 
+        //Tlaèítko pro sèítání, rozhodla jsem se celou kalkulaèku udìlat na desetinná èísla - double
         {
             prvni = Convert.ToDouble(textBox1.Text);
             double druhe = Convert.ToDouble(textBox2.Text);
@@ -22,9 +18,10 @@ namespace KalkulackaInteraktiv
         }
 
         private void buttonOdcitani_Click(object sender, EventArgs e)
+        //Mezi tlaèítkem sèítání, odèítání, násobení a dìlení je jen jeden rozdíl - ve znaménku ve tøetím kroce.
         {
             prvni = Convert.ToDouble(textBox1.Text);
-           double druhe = Convert.ToDouble(textBox2.Text);
+            double druhe = Convert.ToDouble(textBox2.Text);
             double rozdil = prvni - druhe;
             textBoxResult.Text = rozdil.ToString();
         }
@@ -49,28 +46,66 @@ namespace KalkulackaInteraktiv
         {
             prvni = Convert.ToDouble(textBox1.Text);
             double druhe = Convert.ToDouble(textBox2.Text);
-            double mocnina = Math.Pow(prvni, druhe); //Matematická funkce Math.Pow (power) nám umocní první èíslo na druhé
+            double mocnina = Math.Pow(prvni, druhe); //Matematická funkce Math.Pow (power) nám umocní první èíslo na druhé.
             textBoxResult.Text = mocnina.ToString();
         }
 
         private void buttonOdmocnina_Click(object sender, EventArgs e)
+        //U druhé odmocniny nám staèí jedno èíslo, a to sice do prvního textBoxu, uživatel to má napsané v instrukcích.
         {
             prvni = Convert.ToDouble(textBox1.Text);
-            double odmocnina = Math.Sqrt(prvni);
-            textBoxResult.Text = odmocnina.ToString();
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-           //Omyl, ale bojím se to odstranit, aby se mi nepokazil kód
+            if (prvni >= 0) //Musíme dát podmínku, že èíslo musí být vìtší nebo rovno 0, protože záporné èíslo nemùžeme odmocnit
+            {
+                double odmocnina = Math.Sqrt(prvni);
+                //Matematická funkce Math.Sqrt (square root) nám vrací kladnou hodnotu odmocniny èísla, proto doplnìna o MessageBox, kde je øeèeno, že musíme poèítat i se zápornou možností.
+                textBoxResult.Text = odmocnina.ToString();
+                MessageBox.Show($"Nezapomeò, že øešení je i v záporných èíslech, a to sice - {odmocnina}");
+            }
+            else
+            {
+                MessageBox.Show($"Nemùžeš odmocòovat záporné èíslo...");
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (int.TryParse(textBox1.Text, out int prvni) && prvni >= 0) //Zde jsem se nauèila nìco o funkci Parse a TryParse díky chatGPT
+            if (int.TryParse(textBox1.Text, out int prvni) && prvni >= 0)
+                //Zde jsem se nauèila nìco o funkci TryParse díky ChatGPT
+                //TryParse nám pøijme øetìzec a snaží se jej pøevést na èíselný datový typ, pokud èíslo splòuje podmínky
+                
             {
-                string binarniCislo = Convert.ToString(prvni, 2);
+                string binarniCislo = Convert.ToString(prvni, 2); //Pøevod na binární èíslo - pomoc díky ChatGPT
                 textBoxResult.Text = binarniCislo.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Špatnì zadané èíslo, zkus to znovu."); //Pokud uživatel zadá špatnì èíslo
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int prvni) && prvni>=0 )
+            {
+                string hexadecimalniCislo = Convert.ToString(prvni, 16); //Pøevod na hexadecimální èíslo, ostejnì jako u binárního, ale místo 2 píšu 16
+                textBoxResult.Text = hexadecimalniCislo;
+            }
+            else
+            {
+                MessageBox.Show("Špatnì zadané èíslo, zkus to znovu."); //Pokud uživatel zadá špatnì èíslo
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           if (int.TryParse(textBox1.Text, out int prvni) && prvni >= 0)
+            {
+                string oktalniCislo = Convert.ToString(prvni, 8);//To stejné jako u dvou pøevodù nahoøe, ale pro oktální, píšu tedy 8
+                textBoxResult.Text = oktalniCislo;
+            }
+           else
+            {
+                MessageBox.Show("Špatnì zadané èíslo, zkus to znovu."); //Pokud uživatel zadá špatnì èíslo
             }
         }
     }
